7a31cd297f0a4876d406f45bc0024ecd4a47c2e6
dinghino <dinghino@users.noreply.github.com>
2025-06-02 12:44:17 +0200

    ♻️ semantic feature card

--- src/components/feature-card/feature-card.tsx
+++ src/components/feature-card/feature-card.tsx
@@ -4,14 +4,19 @@ import { DynamicIcon, IconName } from 'lucide-react/dynamic'
 
 const variants = tv({
   slots: {
-    card: 'flex flex-col flex-start rounded-lg p-4 md:p-8 gap-4',
+    card: ['flex flex-col flex-start p-4 md:p-8 gap-8', 'rounded-lg', 'text-center'],
     title: 'text-lg font-semibold',
-    icon: 'stroke-2',
+    icon: 'stroke-1',
+    header: 'flex flex-col items-center gap-2',
   },
   variants: {
     background: {
-      light: { card: 'bg-brand-green-light text-brand-green-light-foreground' },
-      dark: { card: 'bg-brand-green-alt text-brand-green-alt-foreground' },
+      light: {
+        card: 'bg-brand-green-light text-brand-green-light-foreground',
+      },
+      dark: {
+        card: 'bg-brand-green-alt text-brand-green-alt-foreground',
+      },
     },
   },
   defaultVariants: {
@@ -30,15 +35,19 @@ export const FeatureCard: React.FC<FeatureCardProps> = (props) => {
   const { className, title, children, icon, ...rest } = props
   const classes = variants({ ...rest })
   const description =
-    typeof children === 'string' ? <p className="text-sm font-medium">{children}</p> : children
+    typeof children === 'string' ? (
+      <p className="text-md font-light tracking-wide">{children}</p>
+    ) : (
+      children
+    )
   return (
-    <div className={cn(classes.card(), className)}>
-      <div className="flex flex-col gap-2">
+    <article className={cn(classes.card(), className)}>
+      <header className={cn(classes.header())}>
         <DynamicIcon size={48} name={icon} className={cn(classes.icon())} />
         <h3 className={cn(classes.title())}>{title}</h3>
-      </div>
+      </header>
       {description}
-    </div>
+    </article>
   )
 }
 
